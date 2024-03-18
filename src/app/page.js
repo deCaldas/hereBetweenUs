0d89a18ea5ad973dc04ecc7a24f27459fdf97592
Diego Toro <d.caldascaridad@gmail.com>
2024-03-17 19:41:46 -0500

    Minor fix

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   <main className={styles.main}>
     <head>
       <title>Acá Entre Nos</title>
-      <meta property="og:title" content="Home de Acá Entre Nos" />
+      <meta property="og:title" content="Homepage de Acá Entre Nos" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://decaldas.github.io/hereBetweenUs/" />
       <meta property="og:image" content="./opengraph-image.png" />
