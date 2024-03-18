da1551f0124546946e8bfa561680baaf083cc0a5
Diego Toro <d.caldascaridad@gmail.com>
2024-03-17 20:16:43 -0500

    metadataBase property in metadata export is set for resolving social open graph

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,8 @@ import Description from "../components/description";
 export default function Home() {
   return (
   <main className={styles.main}>
-    <head>
-      <title>Acá Entre Nos</title>
-      <meta property="og:title" content="Home de Acá Entre Nos" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://decaldas.github.io/hereBetweenUs/" />
-      <meta property="og:image" content="./opengraph-image.png" />
-    </head>
     <Description />
     <Grid />
-
   </main>
   );
 }
