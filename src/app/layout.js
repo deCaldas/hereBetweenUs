b17ca994d6cd27067113231bfc4faf3f8c93a71b
Diego Toro <d.caldascaridad@gmail.com>
2024-03-17 21:02:43 -0500

    configured metadataBase

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,16 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Acá Entre Nos",
-  description: "Dos influencers virtuales que te acompañan hacia una masculinidad moderna."
+  description: "Dos influencers virtuales que te acompañan hacia una masculinidad moderna.",
+  metadataBase: new URL('https://decaldas.github.io/hereBetweenUs/'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'es-CO': '/es-CO',
+    },
+  },
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+    <html>
       <body className={inter.className}>
         <Header />
         {children}
